558f6dbd03aff28a6c5080068a712bacf763c9f7
Elena Datsenko <Elenasheglova15414@Gmail.com>
2023-02-20 00:19:05 +0300

    readme

--- src/CreditPaymentService.java
+++ src/CreditPaymentService.java
@@ -1,15 +1,15 @@
 public class CreditPaymentService {
-    public int calculate(int sumKred, int peritd){
+    public int calculate(int sumKred, int peritd) {
 
         double per;
         double perMonth;
         double bid;
         int n;
-        perMonth = (double) (9.99/(100*12));
-        bid = (1+perMonth);
+        perMonth = (double) (9.99 / (100 * 12));
+        bid = (1 + perMonth);
         n = -peritd;
-        per = (double) Math.pow(bid,n);
-        double pay = (double) (sumKred*(perMonth/(1-per)));
+        per = (double) Math.pow(bid, n);
+        double pay = (double) (sumKred * (perMonth / (1 - per)));
 
         return (int) pay;
     }
